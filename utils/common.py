dfaae2079691d69b61b79d5cdbcca3bec9b22f14
ThuanNaN <duongthuan1445@gmail.com>
2025-11-12 09:57:47 +0000

    feat: add seed initialization function and integrate it into training script

--- utils/common.py
+++ utils/common.py
@@ -1,4 +1,18 @@
+import os
+import random
 import numpy as np
+import torch
+
+def seed_everything(seed: int):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cuda.matmul.fp32_precision = "ieee"
+    torch.backends.cudnn.conv.fp32_precision = "ieee"
 
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
