da547327d209faec8b3d0b6f1e8e953249c75851
ThuanNaN <duongthuan1445@gmail.com>
2025-11-12 09:25:58 +0000

    feat: add EVJVQA dataset loader and update __init__.py for inclusion

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -3,5 +3,6 @@ from .openvivqa import OpenViVQADataset
 from .vivqax import ViVQAXDataset
 from .viocrvqa import ViOCRVQADataset
 from .vitextvqa import ViTextVQADataset
+from .evjvqa import EVJVQADataset
 
-__all__ = ["ViVQADataset", "OpenViVQADataset", "ViVQAXDataset", "ViOCRVQADataset", "ViTextVQADataset"]
+__all__ = ["ViVQADataset", "OpenViVQADataset", "ViVQAXDataset", "ViOCRVQADataset", "ViTextVQADataset", "EVJVQADataset"]
