599bc338e3a600577501c33ebe9fb49372d5e31d
ThuanNaN <duongthuan1445@gmail.com>
2025-11-12 09:10:46 +0000

    feat: add ViOCRVQA dataset loader and update __init__.py for inclusion

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,5 +1,6 @@
 from .vivqa import ViVQADataset
 from .openvivqa import OpenViVQADataset
 from .vivqax import ViVQAXDataset
+from .viocrvqa import ViOCRVQADataset
 
-__all__ = ["ViVQADataset", "OpenViVQADataset", "ViVQAXDataset"]
+__all__ = ["ViVQADataset", "OpenViVQADataset", "ViVQAXDataset", "ViOCRVQADataset"]
